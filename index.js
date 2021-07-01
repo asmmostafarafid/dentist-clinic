f3acd5bdb60ab5368645173391f4e04dac350d00
rafid365.info@gmail.com <rafid365.info@gmail.com>
2021-07-01 16:33:33 +0600

    fixed port

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const fs = require("fs-extra");
 const fileUpload = require("express-fileupload");
 const MongoClient = require("mongodb").MongoClient;
+const port = process.env.PORT || 5000;
 require("dotenv").config();
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ugvqo.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
